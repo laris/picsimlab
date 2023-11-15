018dd7bdd8ff78b717615f43471d14e34df5209f
Laris Qiao <laris.qiao@gmail.com>
2023-11-15 20:57:02 +0800

    rename and add some steps to remove build files and clean files

--- clean_uninstall_macos.sh
+++ clean_uninstall_macos.sh
@@ -2,12 +2,22 @@
 
 cd build_all/
 
+cd lunasvg
+rm -rf lunasvg
+cd build
+make clean
+cd ..
+cd ..
 # uninstall lxrad
 cd lxrad
+rm -rf lunasvg
+make clean
 make uninstall
 cd .. # back to build_all
 
 # uninstall/rm simavr
+cd simavr
+make clean
 DESTDIR="/usr/local"
 rm -rf $DESTDIR/include/simavr
 rm -rf $DESTDIR/lib/libsimavr.a
@@ -21,19 +31,23 @@ rm -rf /usr/local/bin/tty0tty
 cd .. # back to main
 # uninstall picsimlab
 cd src
+make clean
 make uninstall
 
 cd ../tools
 
 cd PinViewer/
+make clean
 make uninstall
 cd ..
 
 cd espmsim/
+make clean
 make uninstall
 cd ..
 
 cd srtank/
+make clean
 make uninstall
 cd ..
 
